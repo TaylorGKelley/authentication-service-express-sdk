73bded4f88443199e50ba8defc1fea6d99ec103c
Taylor Kelley <taylorgkelley@outlook.com>
2025-04-21 12:43:22 -0500

    feat: added type config parameter for constructor initialization

--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
-export default class authService {
-  constructor() {
+export default class AuthService {
+  constructor(authConfig: AuthServiceConfig) {
     console.log('Hello from the test function');
   }
 }
+
+export type AuthServiceConfig = {
+  clientId: string;
+  serviceBaseUrl: `${'http://' | 'https://'}${string}`;
+};
