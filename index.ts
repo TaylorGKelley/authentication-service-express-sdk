7d8fb1321ff9a1814dcec2f3bf91b3af0c6b8413
Taylor Kelley <taylorgkelley@outlook.com>
2025-04-21 12:51:40 -0500

    feat: Added express types and authorize RequestHandler

--- index.ts
+++ index.ts
@@ -1,7 +1,17 @@
+import { RequestHandler } from 'express';
+
 export default class AuthService {
+  private _clientId: string;
+  private _baseUrl: string;
+
   constructor(authConfig: AuthServiceConfig) {
-    console.log('Hello from the test function');
+    this._clientId = authConfig.clientId;
+    this._baseUrl = authConfig.serviceBaseUrl;
   }
+
+  public authorize: RequestHandler = (req, res, next) => {
+    console.log(this._clientId);
+  };
 }
 
 export type AuthServiceConfig = {
