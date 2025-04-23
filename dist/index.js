0c5f9a082e24846bbd8a43ee7a032a3fe68f8c7d
Taylor Kelley <taylorgkelley@outlook.com>
2025-04-23 09:07:55 -0500

    fix: handle axios errors

--- dist/index.js
+++ dist/index.js
@@ -37,31 +37,39 @@ var import_axios = __toESM(require("axios"));
 var clientId = "8d46d402-37e4-4b9c-82ef-ccf44acbb43f";
 function authorize(allowedPermissions) {
   return async (req, res, next) => {
-    var _a;
-    const token = (_a = req.headers.authorization) == null ? void 0 : _a.split(" ").at(1);
-    if (!token) {
-      res.status(401).json({ message: "Unauthorized" });
-      return;
-    }
-    const response = await import_axios.default.get(
-      `http://localhost:7001/api/v1/user-permissions/${clientId}`,
-      {
-        headers: req.headers
+    var _a, _b;
+    try {
+      const token = (_a = req.headers.authorization) == null ? void 0 : _a.split(" ").at(1);
+      if (!token) {
+        res.status(401).json({ message: "Unauthorized" });
+        return;
       }
-    );
-    if (response.status !== 200) {
-      res.status(response.status).json(response.data);
-      return;
+      const response = await import_axios.default.get(
+        `http://localhost:7001/api/v1/user-permissions/${clientId}`,
+        {
+          headers: req.headers
+        }
+      );
+      if (response.status !== 200) {
+        res.status(response.status).json(response.data);
+        return;
+      }
+      const { user, permissions } = response.data;
+      req.user = user;
+      const isAllowed = allowedPermissions.some(
+        (allowedPermission) => permissions.includes(allowedPermission)
+      );
+      if (!isAllowed && !allowedPermissions.includes("public")) {
+        res.status(403).json({ message: "Forbidden" });
+      }
+      next();
+    } catch (error) {
+      if (import_axios.default.isAxiosError(error)) {
+        res.status((_b = error.status) != null ? _b : 500).json({ message: error.message });
+      } else {
+        res.status(500).json({ message: error.message });
+      }
     }
-    const { user, permissions } = response.data;
-    req.user = user;
-    const isAllowed = allowedPermissions.some(
-      (allowedPermission) => permissions.includes(allowedPermission)
-    );
-    if (!isAllowed && !allowedPermissions.includes("public")) {
-      res.status(403).json({ message: "Forbidden" });
-    }
-    next();
   };
 }
 // Annotate the CommonJS export names for ESM import in node:
--- src/index.ts
+++ src/index.ts
@@ -6,38 +6,46 @@ const clientId = '8d46d402-37e4-4b9c-82ef-ccf44acbb43f';
 
 export function authorize(allowedPermissions: string[]): RequestHandler {
   return async (req, res, next) => {
-    const token = req.headers.authorization?.split(' ').at(1);
+    try {
+      const token = req.headers.authorization?.split(' ').at(1);
 
-    if (!token) {
-      res.status(401).json({ message: 'Unauthorized' });
-      return;
-    }
-
-    // axios request
-    const response = await axios.get<UserPermissionResponse>(
-      `http://localhost:7001/api/v1/user-permissions/${clientId}`,
-      {
-        headers: req.headers,
+      if (!token) {
+        res.status(401).json({ message: 'Unauthorized' });
+        return;
       }
-    );
 
-    if (response.status !== 200) {
-      res.status(response.status).json(response.data);
-      return;
+      // axios request
+      const response = await axios.get<UserPermissionResponse>(
+        `http://localhost:7001/api/v1/user-permissions/${clientId}`,
+        {
+          headers: req.headers,
+        }
+      );
+
+      if (response.status !== 200) {
+        res.status(response.status).json(response.data);
+        return;
+      }
+
+      const { user, permissions } = response.data;
+      req.user = user;
+
+      // Check if user is in allowed roles
+      const isAllowed = allowedPermissions.some((allowedPermission) =>
+        permissions.includes(allowedPermission)
+      );
+
+      if (!isAllowed && !allowedPermissions.includes('public')) {
+        res.status(403).json({ message: 'Forbidden' });
+      }
+
+      next();
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        res.status(error.status ?? 500).json({ message: error.message });
+      } else {
+        res.status(500).json({ message: (error as Error).message });
+      }
     }
-
-    const { user, permissions } = response.data;
-    req.user = user;
-
-    // Check if user is in allowed roles
-    const isAllowed = allowedPermissions.some((allowedPermission) =>
-      permissions.includes(allowedPermission)
-    );
-
-    if (!isAllowed && !allowedPermissions.includes('public')) {
-      res.status(403).json({ message: 'Forbidden' });
-    }
-
-    next();
   };
 }
